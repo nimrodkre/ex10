78a2c186a19eb063202ff6ebe604b7e5bc101cd2
nimrodkre <nimrod.kremer@mail.huji.ac.il>
2019-12-29 14:09:39 +0200

    Asteroides added, and are now allowed to collide

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 from game_piece import GamePiece
+import math
+
 
 class Asteroid(GamePiece):
     def __init__(self, x, y, size, speed_x, speed_y):
@@ -14,6 +16,20 @@ class Asteroid(GamePiece):
         if type(size) != int:
             raise TypeError("Expected int")
         if size < 1 or size > 3:
-            raise ValueError("Torpedo size between 1 and 3")
+            raise ValueError("Asteroid size between 1 and 3")
 
         self.__size = size
+
+    def calculate_radius(self):
+        return self.size * 10 - 5
+
+    def has_intersection(self, obj):
+        """
+        checks if the given object and the asteroid have collided
+        :param obj: the object to check
+        :return:
+        """
+        distance = math.sqrt(math.pow((obj.x - self.x), 2) +
+                             math.pow((obj.y - self.y), 2))
+
+        return distance <= self.calculate_radius() + obj.radius
--- ship.py
+++ ship.py
@@ -6,10 +6,15 @@ class Ship(GamePiece):
     def __init__(self, x, y, heading, speed_x, speed_y):
         GamePiece.__init__(self, x, y, speed_x, speed_y)
         self.__heading = heading
+        self.__radius = 1
 
     @property
     def heading(self):
         return self.__heading
+
+    @property
+    def radius(self):
+        return self.__radius
 
     @heading.setter
     def heading(self, heading):
@@ -18,8 +23,8 @@ class Ship(GamePiece):
     def change_heading(self, side):
         """
         changes the heading of the ship
-        :param side: if 1 than right, and change 7 degrees clockwise
-                     if 0 than left, and change 7 degrees counter-clockwise
+        :param side: if 0 than right, and change 7 degrees clockwise
+                     if 1 than left, and change 7 degrees counter-clockwise
         :return: None
         """
         if side:
